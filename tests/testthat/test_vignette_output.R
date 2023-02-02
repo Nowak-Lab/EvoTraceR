260c1b525a101649041b4fa96bf72ff9931765e0
Rebecca Hassett <hassett@cshl.edu>
2023-02-02 14:52:15 -0500

    Update vignette test

--- tests/testthat/test_vignette_output.R
+++ tests/testthat/test_vignette_output.R
@@ -1,17 +1,10 @@
 data("EvoTraceR_object")
 
-phylogeny_output_path = "../../output/phylogeny_analysis/phylogeny_del_ins/"
-asv_stats_file = "asv_stat.csv"
-tree_clones_csv = "tree_all_clones.csv"
-tree_clones_newick = "tree_all_clones.newick"
+test_that("EvoTraceR vignette produces expected starting ASVs", {
+    expect_equal(EvoTraceR_object$preprocessing$seq_filters$num[1], 12241)
+})
 
-test_that("EvoTraceR vignette produces correct output", {
-    announce_snapshot_file(name = asv_stats_file)
-    expect_snapshot_file(paste(phylogeny_output_path, asv_stats_file, sep=''), name=asv_stats_file)
-    announce_snapshot_file(name = tree_clones_csv)
-    expect_snapshot_file(paste(phylogeny_output_path, tree_clones_csv, sep=''), name=tree_clones_csv)
-    announce_snapshot_file(name = tree_clones_newick)
-    expect_snapshot_file(paste(phylogeny_output_path, tree_clones_newick, sep=''), name=tree_clones_newick)
-    expect_snapshot(EvoTraceR_object$preprocessing)
-    expect_snapshot(EvoTraceR_object$alignment)
+
+test_that("EvoTraceR vignette produces expected final ASVs", {
+    expect_true(EvoTraceR_object$preprocessing$seq_filters$num[6] >= 707 & EvoTraceR_object$preprocessing$seq_filters$num[6] <= 709)
 })
